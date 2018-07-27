e5e91876318d7009322ef35c3c5e71c6f46909e9
Jeff Han <jeff1fbc@gmail.com>
2018-07-27 02:34:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+
+	function takeANumber(katzDeliLine, name)
+        {
+          
+          var position
+          
+          position=katzDeliLine.length
+          console.log(`"Welcome,"+ name+". You are number $(position) in line."`)
+
+
+           
+        }
